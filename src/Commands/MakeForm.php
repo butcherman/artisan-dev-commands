d6078cdfb5d0c58265e4577ce425feb7132e5438
butcherman <ronbutcherii@gmail.com>
2025-01-20 18:37:23 -0800

    Add Make Form command

--- src/Commands/MakeForm.php
+++ src/Commands/MakeForm.php
@@ -4,40 +4,40 @@ namespace Butcherman\ArtisanDevCommands\Commands;
 
 use Illuminate\Console\Command;
 
-class MakePage extends Command
+class MakeForm extends Command
 {
     /**
      * @var string
      */
-    protected $signature   = 'make:page
-                            {name : Name of the page to be created}
-                            {--optionsApi : If added, the Vue Options API will be suggested rather than the Composition API}';
+    protected $signature = 'make:form
+                            {name : Name of form to be created}
+                            {--optionsApi : If added, the vue Options API will be suggested rather than Composition API}';
 
     /**
      * @var string
      */
-    protected $description = 'Create a new Vue Page in the resources/js/Pages folder';
+    protected $description = 'Create a new blank form template in the resources/js/Forms folder';
 
     /**
-     * Create a new page in the /resources/js/Pages folder.  Create directories
-     * as needed.
+     * Create a new blank form in the /resources/js/Forms folder.  Create all
+     * directories as needed
      */
     public function handle()
     {
         //  Get the name and path the Page will be placed
         $nameArr  = explode('.', $this->argument('name'));
-        $basePath = resource_path('js') . DIRECTORY_SEPARATOR . 'Pages';
+        $basePath = resource_path('js') . DIRECTORY_SEPARATOR . 'Forms';
         $path     = $basePath . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $nameArr) . '.vue';
         $pathInfo = pathinfo($path);
 
-        //  Determine if the /resources/Pages directory exists - if not, create it
+        //  Determine if the /resources/Forms directory exists - if not, create it
         if (!is_dir($basePath)) {
             mkdir($basePath);
         }
 
-        //  If the page already exists, we will trigger an error
+        //  If the form already exists, we will trigger an error
         if (file_exists($path)) {
-            $this->error('This page already exists');
+            $this->error('A form with this name already exists');
             return 0;
         }
 
@@ -46,7 +46,7 @@ class MakePage extends Command
         }
 
         copy($this->getStub(), $path);
-        $this->info('New page created successfully');
+        $this->info('New form created successfully');
 
         return 0;
     }
@@ -54,7 +54,7 @@ class MakePage extends Command
     //  Get the stub file for the generator.
     protected function getStub()
     {
-        $stubName = $this->option('optionsApi') ? 'PageStub-Options.stub' : 'PageStub-Setup.stub';
+        $stubName = $this->option('optionsApi') ? 'FormStub-Options.stub' : 'FormStub-Setup.stub';
 
         if (file_exists(base_path('stubs/dev_commands/' . $stubName))) {
             return base_path('stubs/dev_commands/' . $stubName);
